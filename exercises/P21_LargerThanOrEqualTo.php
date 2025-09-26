7182bac07ab2163baad3f5c93cb61f5d55ca732a
aniol <aniolnicolauroig@gmail.com>
2025-09-26 18:28:45 +0200

    EX22

--- exercises/P21_LargerThanOrEqualTo.php
+++ exercises/P21_LargerThanOrEqualTo.php
@@ -6,13 +6,20 @@ class P21_LargerThanOrEqualTo
     {
         // Write your code here
         // Prompt the user for input
-
+        echo "Give the first number: \n";
+        $firstNumberInput = trim(string: fgets(stream: $GLOBALS['STDIN'] ?? STDIN));
         // Get input from the user
-        
+        echo "Give the second number: \n";
+        $secondNumberInput = trim(string: fgets(stream: $GLOBALS['STDIN'] ?? STDIN));
         // Prompt the user for input
-        
         // Get input from the user
-
         // Check year value
+        if ((int)$firstNumberInput > (int)$secondNumberInput) {
+            echo "\n Greater number is: " . $firstNumberInput . "\n";
+        } elseif ((int)$firstNumberInput < (int)$secondNumberInput) {
+            echo "\n Greater number is: " . $secondNumberInput . "\n";
+        } else {
+            echo "\n The numbers are equal!\n";
+        }
     }
 }
