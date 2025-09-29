9ea735ae99a740f5a8c22d94e08356dd9fcbed2f
aniol <aniolnicolauroig@gmail.com>
2025-09-29 19:05:01 +0200

    EX 26, 27

--- exercises/P26_Same.php
+++ exercises/P26_Same.php
@@ -5,6 +5,14 @@ class P26_Same
     public function main(): void
     {
         // Write your code here
-       
+        echo "Enter the first string: ";
+        $input1 = trim(string: fgets(stream: $GLOBALS['STDIN'] ?? STDIN));
+        echo "Enter the second string: ";
+        $input2 = trim(string: fgets(stream: $GLOBALS['STDIN'] ?? STDIN));
+        if ($input1 == $input2) {
+            echo "Same";
+        } else {
+            echo "Different";
+        }
     }
 }
--- exercises/P27_CheckingTheAge.php
+++ exercises/P27_CheckingTheAge.php
@@ -5,6 +5,12 @@ class P27_CheckingTheAge
     public function main(): void
     {
         // Write your code here
-       
+        echo "How old are you? ";
+        $age = trim(string: fgets(stream: $GLOBALS['STDIN'] ?? STDIN));
+        if ($age < 0 || $age > 120) {
+            echo "Impossible!";
+        } else {
+            echo "Ok";
+        }
     }
 }
