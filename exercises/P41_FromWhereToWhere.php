f9788b752fdb463e7db42953c79267a8e26077fa
aniol <aniolnicolauroig@gmail.com>
2025-10-01 18:03:36 +0200

    EX 41, 42

--- exercises/P41_FromWhereToWhere.php
+++ exercises/P41_FromWhereToWhere.php
@@ -5,6 +5,14 @@ class P41_FromWhereToWhere
     public function main(): void
     {
         // Write your program here
-       
+        echo "Where to?";
+        $where_to = (int) trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        echo "Where from?";
+        $where_from = (int) trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        if ($where_to >= $where_from) {
+            for ($i = $where_from; $i <= $where_to; $i++) {
+                echo $i . "\n";
+            }
+        }
     }
 }
